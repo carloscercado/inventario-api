54e9f94280697bf323828db8092f3e55f484b65e
Carlos Cercado <cercadocarlos@gmail.com>
2017-10-20 07:48:36 -0400

    Creacion del modelo Unidad
    Creacion del modelo de unidad en el modulo de ubicacion.

--- src/ubicacion/models.py
+++ src/ubicacion/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from compras.models import DetalleCompra
 
 class Almacen(models.Model):
     nombre = models.CharField(max_length=30, help_text="nombre del almacen")
@@ -16,3 +17,13 @@ class Estante(models.Model):
                                   related_name="estantes")
     capacidad_total = models.FloatField(default=0, help_text="capacidad del almacen")
     capacidad_restante = models.FloatField(help_text="capacidad disponible en el almacen")
+
+class Unidad(models.Model):
+    unidad = models.ForeignKey(DetalleCompra, related_name="unidades",
+                               help_text="producto a ubicar",
+                               on_delete=models.CASCADE)
+    codigo = models.CharField(max_length=30, help_text="codigo unico de la unidad")
+    estado = models.BooleanField(default=True, null=True, help_text="Disponible/Usado")
+    estante = models.ForeignKey(Estante, on_delete=models.CASCADE,
+                                help_text="Estante donde se ubicará la unidad",
+                                related_name="unidades")
