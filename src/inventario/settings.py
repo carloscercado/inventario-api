a78194a9158227c64c2231a673f97fd74ef552da
Carlos Cercado <cercadocarlos@gmail.com>
2017-11-01 09:38:07 -0400

    Arreglo de setting y datos de acceso

--- src/inventario/settings.py
+++ src/inventario/settings.py
@@ -88,6 +88,7 @@ WSGI_APPLICATION = 'inventario.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/1.11/ref/settings/#databases
 
+
 DATABASES = {
         'default': {
             'ENGINE': 'django.db.backends.postgresql_psycopg2', # Add 'postgresql_psycopg2', 'mysql', 'sqlite3' or 'oracle'.
